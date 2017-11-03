42d31268baa9c35987e21c33a1af9abb2e0b62b8
Benoit Beausejour <b@turbulent.ca>
2017-11-02 23:00:24 -0400

    substance home dir

--- scripts/alpine/substance.sh
+++ scripts/alpine/substance.sh
@@ -10,11 +10,15 @@ DOCKER_HOST="tcp://127.0.0.1:2375"
 EOFPROFILE
 chown -R substance:substance /substance/.profile
 
+mkdir -p /substance/devroot
+chown -R substance:substance /substance/devroot
+
 mkdir -pm 700 /substance/.ssh
 curl -sSo /substance/.ssh/authorized_keys 'https://raw.githubusercontent.com/turbulent/substance/master/support/substance_insecure.pub'
 chown -R substance:substance /substance/.ssh
 chmod -R go-rwsx /substance/.ssh
 adduser substance docker
+mkdir -p /substance/devroot
 
 echo "Defaults env_keep += SSH_AUTH_SOCK" >> /etc/sudoers
 echo "substance ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/substance
