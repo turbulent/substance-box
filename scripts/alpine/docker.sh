543761847cf5c22f96f0f11f3cd2c3d93a7f3ee4
Benoit Beausejour <b@turbulent.ca>
2017-11-04 16:28:35 -0400

    Use overlay2 storage driver instead of overlay to fix inode exhaustion issue.

--- scripts/alpine/docker.sh
+++ scripts/alpine/docker.sh
@@ -3,6 +3,6 @@ apk add docker
 rc-update add docker boot
 
 cat << EOF >> /etc/conf.d/docker
-DOCKER_OPTS="--userland-proxy=false --storage-driver=overlay -H 0.0.0.0:2375 -H unix:///var/run/docker.sock"
+DOCKER_OPTS="--userland-proxy=false --storage-driver=overlay2 -H 0.0.0.0:2375 -H unix:///var/run/docker.sock"
 
 EOF
